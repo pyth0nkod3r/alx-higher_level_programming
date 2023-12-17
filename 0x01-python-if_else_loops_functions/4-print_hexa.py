a8463962c6f1544df751296f378c9bb88e4f22e3
Miracle Anyanwu <o.amagwula@stu.unizik.edu.ng>
2023-12-17 22:27:01 +0100

    if/else

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python
+for numbers in range(99):
+    print(f"{numbers} = 0x{numbers:x}")
---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+for numbers in range(0, 98):
+    print(f"{numbers:02d}, ", end="")
+print(99)
