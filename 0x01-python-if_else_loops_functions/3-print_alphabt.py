5674604728f2a71a29bea2fa6028143de4edad7e
Miracle Anyanwu <o.amagwula@stu.unizik.edu.ng>
2023-12-18 13:12:19 +0100

    if/else

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,4 +2,4 @@
 for alphabet in range(97, 123):
     if chr(alphabet) == 'q' or chr(alphabet) == 'e':
         continue
-    print("".join(chr(alphabet)), end='')
+    print("{}".format(chr(alphabet)), end='')
