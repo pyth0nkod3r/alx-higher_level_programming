4eefd460182ab91e7695b1185af00e7ce7d15b1b
Miracle Anyanwu <o.amagwula@stu.unizik.edu.ng>
2023-12-19 15:31:10 +0100

    if/else

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -5,7 +5,7 @@ for num1 in range(10):
     for num2 in range(num1 + 1, 10):
         # Print the two digits with a comma and a space after each one
         if num1 == 8 and num2 == 9:
-            print("{}{}".format(num1, num2))
+            print("{}{}".format(num1, num2, end=""))
         else:
             print("{}{}".format(num1, num2), end=", ")
 # Print a new line
