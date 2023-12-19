b688ee3524796bc354513e2948d2a897350eb0f2
Miracle Anyanwu <o.amagwula@stu.unizik.edu.ng>
2023-12-19 15:26:09 +0100

    if/else

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,6 +4,9 @@ for num1 in range(10):
     # Loop from i + 1 to 9 for the second digit
     for num2 in range(num1 + 1, 10):
         # Print the two digits with a comma and a space after each one
-        print("{}{}".format(num1, num2), end=", ")
+        if num1 == 8 and num2 == 9:
+            print("{}{}".format(num1, num2))
+        else:
+            print("{}{}".format(num1, num2), end=", ")
 # Print a new line
 print()
