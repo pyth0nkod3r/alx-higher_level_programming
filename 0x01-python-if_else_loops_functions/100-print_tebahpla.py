79acb720e79ac8b3ca8d56840a06d354bc89b4f8
Miracle Anyanwu <o.amagwula@stu.unizik.edu.ng>
2024-01-03 05:58:15 +0100

    If/Else

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 for i in range(ord('z'), ord('a') - 1, -1):
-    if i % 2 == 0 :
+    if i % 2 == 0:
         x = 0
     else:
         x = 32
     print("{}".format(chr(i - x)), end="")
-    
