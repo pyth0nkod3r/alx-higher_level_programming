344d65fde2f5f923a52e39bd3bca0bfbe79f2bcf
Miracle Anyanwu <o.amagwula@stu.unizik.edu.ng>
2024-04-30 17:37:35 +0100

    0x03

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    word = []
+    word.append(my_string)
+    for char in word:
+        if char == 'c' or char == 'C':
+            word.remove(char)
+        return word
