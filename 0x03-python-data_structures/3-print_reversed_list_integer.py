643daf35affa3739c9fa822690d1a73bf34b9bd2
Miracle Anyanwu <o.amagwula@stu.unizik.edu.ng>
2024-04-30 17:06:47 +0100

    0x03

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
-    for i in my_list:
+    new_list = my_list
+    new_list.reverse()
+    for i in new_list:
         print("{:d}".format(i))
