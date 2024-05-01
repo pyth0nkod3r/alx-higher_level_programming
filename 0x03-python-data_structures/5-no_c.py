47755e69c138beb74b448fed6f80b3b2189d5a47
Miracle Anyanwu <o.amagwula@stu.unizik.edu.ng>
2024-05-01 04:39:11 +0100

    0x03

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,10 +3,10 @@
 def no_c(my_string):
     word = []
     word.append(my_string)
-    for char in word:
-        if char == 'c' or char == 'C':
-            word.remove(char)
-    new_str = ''
-    for element in word:
-        new_str += element
-    return element
+    unwanted = ['c', 'C']
+    for each_word in word:
+        new_word = ""
+        for char in each_word:
+            if char not in unwanted:
+                new_word += char
+        return new_word
