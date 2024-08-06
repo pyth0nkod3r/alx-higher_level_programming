501566e2c253f612be9a16b5fda5f64568db55fc
Miracle Anyanwu <o.amagwula@stu.unizik.edu.ng>
2024-08-06 05:07:08 +0100

    task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if not sentence:
-        return None, None
+    if sentence == "":
+        sentence = None
+        return (None, sentence)
     else:
         return len(sentence), sentence[0]
