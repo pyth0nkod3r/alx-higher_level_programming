0a3e9867a040ae5c21fef718a023d3e4a1b0b12e
Miracle Anyanwu <o.amagwula@stu.unizik.edu.ng>
2024-08-22 16:06:17 +0100

    added more tasks

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,23 @@
 #!/usr/bin/python3
+
 def multiple_returns(sentence):
+    """
+    This function takes a sentence as input.
+    If the sentence is an empty string, it returns a tuple with a None value and the same empty string.
+    If the sentence is not an empty string, it returns a tuple with the length of the sentence and the first character of the sentence.
+    """
+
+    # Check if the input sentence is an empty string
     if sentence == "":
+        # If it is, set the sentence variable to None
         sentence = None
+        # Return a tuple with a None value and the same empty string
         return (None, sentence)
+    # If the input sentence is not an empty string
     else:
-        return len(sentence), sentence[0]
+        # Get the length of the sentence
+        length = len(sentence)
+        # Get the first character of the sentence
+        first_char = sentence[0]
+        # Return a tuple with the length of the sentence and the first character of the sentence
+        return (length, first_char)
