ded58ac8ad2b42f7bf8b213cb593920ba37cbf46
Miracle Anyanwu <o.amagwula@stu.unizik.edu.ng>
2024-08-23 13:59:34 +0100

    added tasks 11-15

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,10 +2,15 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
-    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+    roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     res = 0
+
     for i in range(len(roman_string)):
-        if i + 1 < len(roman_string) and roman[roman_string[i]] < roman[roman_string[i + 1]]:
+        if (
+            i + 1 < len(roman_string)
+            and roman[roman_string[i + 1]] > roman[roman_string[i]]
+        ):
             res -= roman[roman_string[i]]
         else:
             res += roman[roman_string[i]]
