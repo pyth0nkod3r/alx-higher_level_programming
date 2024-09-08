45e9e168d1cf15986af7b4d728558de6935943d3
Miracle Anyanwu <o.amagwula@stu.unizik.edu.ng>
2024-09-08 04:10:59 +0100

    Classes and objects

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a square"""
+
+
+class Square:
+    """
+    This class defines a square
+    """
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a square"""
+class Square:
+    """Represents a square"""
+    def __init__(self, size):
+        """Initializes a new square
+        Args:
+            size (int): The size of the new square
+        """
+        self.__size = size 
